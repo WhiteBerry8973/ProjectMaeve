5c55f8a0ccb5abac1283962b17f302ba4868533e
APYG-R <attapinya.r@ku.th>
2025-09-18 00:57:36 +0700

    Update Coffee.java
    เป็นส่วนนึงที่ทำให้ gui สร้าง auto ได้

--- model/Coffee.java
+++ model/Coffee.java
@@ -1,8 +1,31 @@
 package model;
 
-public  interface Coffee {
-    String getName();
-    int getPrice();
-    String getDescription();
-    String getImagePath();
+public class Coffee {
+    private String name;
+    private double price;
+    private String description;
+    private String imagePath;
+
+    public Coffee(String name, double price, String description, String imagePath) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
 }
