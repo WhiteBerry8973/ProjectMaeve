61124088982dee921da447db8d86af2b42a67665
WhiteBerry8973 <natthachai.ru@ku.th>
2025-09-19 19:19:51 +0700

    Up

--- Test.java
+++ Test.java
@@ -7,6 +7,8 @@ public class Test {
         DiscountStrategy discountedPrice = new PointToCashDiscount();
         double finalPrice = 0;
 
+        System.out.println("------------------------------------------------------------------------");
+
         Coffee coffee = FactoryMethodPattern.CoffeeFactory.createCoffee("hot", "Espresso", 50);
         System.out.println("Coffee: " + coffee.getName() + ", Price: " + coffee.getPrice() +" baht");
         coffee = new DecoratorMethodPattern.WhippingcreamDecorator(coffee);
@@ -14,14 +16,18 @@ public class Test {
         coffee = new DecoratorMethodPattern.MarshmallowDecorator(coffee);
         System.out.println("Coffee: " + coffee.getName() + ", Price: " + coffee.getPrice() +" baht");
         finalPrice = discountedPrice.applyDiscount(coffee.getPrice(), 300); // 300 points
-        System.out.println("Final Price after discount: " + finalPrice + " baht\n");
+        System.out.println("Final Price after discount: " + finalPrice + " baht");
+
+        System.out.println("------------------------------------------------------------------------");
 
         Coffee iceCoffee = FactoryMethodPattern.CoffeeFactory.createCoffee("ice", "Latte", 60);
         System.out.println("Coffee: " + iceCoffee.getName() + ", Price: " + iceCoffee.getPrice() +" baht");
         iceCoffee = new DecoratorMethodPattern.MarshmallowDecorator(iceCoffee);
         System.out.println("Coffee: " + iceCoffee.getName() + ", Price: " + iceCoffee.getPrice() +" baht");
-        finalPrice = discountedPrice.applyDiscount(iceCoffee.getPrice(), 500); // 500 points
+        finalPrice = discountedPrice.applyDiscount(iceCoffee.getPrice(), 550); // 550 points
         System.out.println("Final Price after discount: " + finalPrice + " baht");
-        
+
+        System.out.println("------------------------------------------------------------------------");
+
     }
 }
