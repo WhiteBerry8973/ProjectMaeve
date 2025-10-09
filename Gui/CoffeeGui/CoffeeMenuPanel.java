69f4308f7099bbd97a662f212cb76fb1ec13b4f1
orangejuice68 <srithep.j@ku.th>
2025-10-10 04:27:28 +0700

    Add files via upload

---
+++ Gui/CoffeeGui/CoffeeMenuPanel.java
@@ -0,0 +1,9 @@
+package Gui.CoffeeGui;
+
+import Gui.MainGui.*;
+
+public class CoffeeMenuPanel extends MenuCatalogPanel {
+    public CoffeeMenuPanel(MaeveCoffeeUI ui) {
+        super(ui, Catalog.COFFEE);
+    }
+}
---
+++ Gui/TeaGui/TeaMenuPanel.java
@@ -0,0 +1,9 @@
+package Gui.TeaGui;
+
+import Gui.MainGui.*;
+
+public class TeaMenuPanel extends MenuCatalogPanel {
+    public TeaMenuPanel(MaeveCoffeeUI ui) {
+        super(ui, Catalog.TEA);
+    }
+}
